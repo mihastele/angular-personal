c780eb471ad11d7706dc8fa11d73ea298344cf2e
Miha Stele <stele1000@gmail.com>
2022-12-22 22:57:48 +0100

    Add 2 inputs

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from "@angular/core";
 import { MatButtonModule } from '@angular/material/button'
 import { MatIconModule } from '@angular/material/icon'
 import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field'
 
 @NgModule({
-    imports: [MatButtonModule, MatIconModule, MatInputModule],
-    exports: [MatButtonModule, MatIconModule, MatInputModule]
+    imports: [MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule],
+    exports: [MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule]
 })
 export class MaterialModule { }
