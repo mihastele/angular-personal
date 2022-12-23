c301f9a98d9320b9dd947f79891c8fbf7d66af22
Miha Stele <stele1000@gmail.com>
2022-12-23 10:26:42 +0100

    Signup form v1

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -6,7 +6,17 @@ import { NgForm } from '@angular/forms';
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit {
+  maxDate;
+
+  ngOnInit() {
+    this.maxDate = new Date()
+
+    const MIN_AGE = 3;
+    this.maxDate.setFullYear(this.maxDate.getFullYear() - MIN_AGE)
+
+  }
+
   onSubmit(form: NgForm) {
     console.log(form)
   }
