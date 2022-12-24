ca71a58a43c183a09dee4041a3861722d4f2876c
Miha Stele <stele1000@gmail.com>
2022-12-24 21:39:19 +0100

    handle login and logout dummy

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,19 +1,26 @@
+import { Injectable } from "@angular/core";
+import { Router } from "@angular/router";
 import { Subject } from "rxjs";
 import { AuthData } from "./auth-data-model";
 import { User } from "./user.model";
 
 
 //rxjs is an event emitter for services and non angular components
+@Injectable() // used to inject other services to this service since it is not an angular component
 export class AuthService {
     private user: User
     authChange = new Subject<boolean>();
+
+    constructor(private router: Router) {
+    }
 
     registerUser(authData: AuthData) {
         this.user = {
             email: authData.email,
             userId: Math.round(Math.random() * 100000).toString() // fake for now
         }
-        this.authChange.next(true); // boolean payload as defined in the Type
+        // this.authChange.next(true); // boolean payload as defined in the Type
+        this.authSuccessfully()
     }
 
 
@@ -22,12 +29,13 @@ export class AuthService {
             email: authData.email,
             userId: Math.round(Math.random() * 100000).toString() // fake for now
         }
-        this.authChange.next(true);
+        this.authSuccessfully()
     }
 
     logout() {
         this.user = null
         this.authChange.next(false);
+        this.router.navigate(['/signup'])
     }
 
     getUser() {
@@ -36,5 +44,10 @@ export class AuthService {
 
     isAuth() {
         return this.user != null;
+    }
+
+    private authSuccessfully() {
+        this.authChange.next(true);
+        this.router.navigate(['/training'])
     }
 }
--- src/app/navigation/header/header.component.ts
+++ src/app/navigation/header/header.component.ts
@@ -25,6 +25,10 @@ export class HeaderComponent implements OnInit, OnDestroy {
     this.subscriptionToAuth.unsubscribe() // clear unneeded memory
   }
 
+  onLogout() {
+    this.authService.logout()
+  }
+
   onToggleSidenav() {
     this.sidenavToggle.emit()
   }
