8b45193069b51f2af52728f78d00273222d56573
Miha Stele <stele1000@gmail.com>
2022-12-24 18:55:16 +0100

    Add initInterval as function

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { WelcomeComponent } from './welcome/welcome.component';
 import { FormsModule } from '@angular/forms';
 import { HeaderComponent } from './navigation/header/header.component';
 import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
+import { StopTrainingComponent } from './training/current-training/stop-training-component';
 
 @NgModule({
   declarations: [
@@ -27,7 +28,8 @@ import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.com
     PastTrainingComponent,
     WelcomeComponent,
     HeaderComponent,
-    SidenavListComponent
+    SidenavListComponent,
+    StopTrainingComponent
   ],
   imports: [
     BrowserModule,
@@ -37,6 +39,7 @@ import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.com
     FormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [StopTrainingComponent] // tells angular to be ready to use it
 })
 export class AppModule { }
--- src/app/training/current-training/current-training.component.ts
+++ src/app/training/current-training/current-training.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+
+import { StopTrainingComponent } from './stop-training-component';
 
 @Component({
   selector: 'app-current-training',
@@ -6,16 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./current-training.component.css']
 })
 export class CurrentTrainingComponent implements OnInit {
-  progress = 0;
-  timer: ReturnType<typeof setInterval> = setInterval(() => {
-    this.progress += 5;
-    if (this.progress >= 100) {
-      clearInterval(this.timer)
-      this.progress = 0;
-    }
-  }, 1000);
 
-  constructor() {
+  initInterval = (secs) => {
+    return setInterval(() => {
+      this.progress += 5;
+      if (this.progress >= 100) {
+        clearInterval(this.timer)
+        this.progress = 0;
+      }
+    }, secs);
+  }
+
+  progress = 0;
+  timer: ReturnType<typeof setInterval> = this.initInterval(1000)
+
+  constructor(private dialog: MatDialog) {
   }
 
   ngOnInit() {
@@ -23,6 +31,9 @@ export class CurrentTrainingComponent implements OnInit {
 
   onStopTimer() {
     clearInterval(this.timer)
-    this.progress = 0;
+    this.dialog.open(StopTrainingComponent)
+    //this.progress = 0;
   }
+
+
 }
