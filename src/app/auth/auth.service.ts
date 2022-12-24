46730273943dbf57c838fefce1a332ab86d54ecb
Miha Stele <stele1000@gmail.com>
2022-12-24 21:31:33 +0100

    Add angular event to handle proper navigation display

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,14 +1,19 @@
+import { Subject } from "rxjs";
 import { AuthData } from "./auth-data-model";
 import { User } from "./user.model";
 
+
+//rxjs is an event emitter for services and non angular components
 export class AuthService {
     private user: User
+    authChange = new Subject<boolean>();
 
     registerUser(authData: AuthData) {
         this.user = {
             email: authData.email,
             userId: Math.round(Math.random() * 100000).toString() // fake for now
         }
+        this.authChange.next(true); // boolean payload as defined in the Type
     }
 
 
@@ -17,10 +22,12 @@ export class AuthService {
             email: authData.email,
             userId: Math.round(Math.random() * 100000).toString() // fake for now
         }
+        this.authChange.next(true);
     }
 
     logout() {
         this.user = null
+        this.authChange.next(false);
     }
 
     getUser() {
--- src/app/navigation/sidenav-list/sidenav-list.component.ts
+++ src/app/navigation/sidenav-list/sidenav-list.component.ts
@@ -1,12 +1,29 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-sidenav-list',
   templateUrl: './sidenav-list.component.html',
   styleUrls: ['./sidenav-list.component.css']
 })
-export class SidenavListComponent {
+export class SidenavListComponent implements OnInit, OnDestroy {
   @Output() closeSidenav = new EventEmitter<void>(); // Output is used to be listenable from outside
+  isAuth: boolean
+  subscriptionToAuth: Subscription
+
+  constructor(private authService: AuthService) {
+  }
+
+  ngOnInit() {
+    this.subscriptionToAuth = this.authService.authChange.subscribe(authStatus => {
+      this.isAuth = authStatus
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptionToAuth.unsubscribe() // clear unneeded memory
+  }
 
   onClose() {
     this.closeSidenav.emit()
