9d6f2856b25c43df92f01dfb7781042689e77306
Miha Stele <stele1000@gmail.com>
2022-12-24 18:39:10 +0100

    20 second timer implement

--- src/app/training/current-training/current-training.component.ts
+++ src/app/training/current-training/current-training.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-current-training',
   templateUrl: './current-training.component.html',
   styleUrls: ['./current-training.component.css']
 })
-export class CurrentTrainingComponent {
+export class CurrentTrainingComponent implements OnInit {
+  progress = 0;
+  timer: ReturnType<typeof setInterval> = setInterval(() => {
+    this.progress += 5;
+    if (this.progress >= 100) {
+      clearInterval(this.timer)
+      this.progress = 0;
+    }
+  }, 1000);
 
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+  onStopTimer() {
+    clearInterval(this.timer)
+    this.progress = 0;
+  }
 }
