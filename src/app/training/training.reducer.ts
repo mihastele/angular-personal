28c1401b840b31ff7ad90ba4bff8dd0b1d2b395e
Miha Stele <stele1000@gmail.com>
2022-12-25 21:50:01 +0100

    Implement a lazy loaded reducer module

---
+++ src/app/training/training.reducer.ts
@@ -0,0 +1,52 @@
+import { Exercise } from "./exercise.model"
+import { SET_AVAILABLE_TRAININGS, SET_FINISHED_TRAININGS, START_TRAINING, STOP_TRAINING, TrainingActions } from "./training.actions"
+import * as fromRoot from '../app.reducer'
+
+export interface TrainingState {
+    availableExercises: Exercise[]
+    finishedExercises: Exercise[]
+    activeTraining: Exercise
+}
+
+export interface State extends fromRoot.State {
+    training: TrainingState
+}
+
+
+const initialState: TrainingState = {
+    availableExercises: [],
+    finishedExercises: [],
+    activeTraining: null
+}
+
+export function authReducer(state = initialState, action: TrainingActions) {
+    switch (action.type) {
+        case SET_AVAILABLE_TRAININGS:
+            return {
+                ...state,
+                availableExercises: action.payload
+            }
+        case SET_FINISHED_TRAININGS:
+            return {
+                ...state,
+                finishedExercises: action.payload
+            }
+        case START_TRAINING:
+            return {
+                ...state,
+                activeTraining: action.payload
+            }
+        case STOP_TRAINING:
+            return {
+                ...state,
+                activeTraining: null
+            }
+        default:
+            return state
+    }
+}
+
+// helper functions
+export const getAvailableExercises = (state: TrainingState) => state.availableExercises
+export const getFinishedExercises = (state: TrainingState) => state.availableExercises
+export const getActiveTraining = (state: TrainingState) => state.activeTraining
