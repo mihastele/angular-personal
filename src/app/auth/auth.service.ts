dfcd659a2ab546026dabe4a07da3f34d8be9cf80
Miha Stele <stele1000@gmail.com>
2022-12-25 17:10:20 +0100

    add image and a snackbar

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -5,6 +5,7 @@ import { AuthData } from "./auth-data-model";
 import { User } from "./user.model";
 import { AngularFireAuth } from "@angular/fire/compat/auth";
 import { TrainingService } from "../training/training.service";
+import { MatSnackBar } from "@angular/material/snack-bar";
 
 
 //rxjs is an event emitter for services and non angular components
@@ -13,7 +14,10 @@ export class AuthService {
     private isAuthenticated: boolean
     authChange = new Subject<boolean>();
 
-    constructor(private router: Router, private auth: AngularFireAuth, private trainingService: TrainingService) {
+    constructor(private router: Router,
+        private auth: AngularFireAuth,
+        private trainingService: TrainingService,
+        private snackBar: MatSnackBar) {
     }
 
     initAuthListener() {
@@ -34,7 +38,10 @@ export class AuthService {
     registerUser(authData: AuthData) {
         this.auth.createUserWithEmailAndPassword(authData.email, authData.password).then(result => {
         }).catch(error => {
-            console.log(error)
+            // 'Error, please check your data or contact administrator'
+            this.snackBar.open(error.message, null, {
+                duration: 3000
+            })
         })
     }
 
@@ -42,7 +49,10 @@ export class AuthService {
     login(authData: AuthData) {
         this.auth.signInWithEmailAndPassword(authData.email, authData.password).then(result => {
         }).catch(error => {
-            console.log(error)
+            // 'Error, most likely invalid credentials'
+            this.snackBar.open(error.message, null, {
+                duration: 3000
+            })
         })
     }
 
